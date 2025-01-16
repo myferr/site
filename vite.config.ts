d7bc42b8543d4cffc9ad5da51c67b9af65093433
MyferIsADev <contactme.myfer@protonmail.com>
2025-01-15 21:42:30 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,6 @@
-import { defineConfig } from "vite";
-import mdx from "@mdx-js/rollup";
-import react from "@vitejs/plugin-react";
-
-// https://vite.dev/config/
-const viteConfig = defineConfig({
-  plugins: [
-    {
-      enforce: "pre",
-      ...mdx({
-        /* jsxImportSource: …, otherOptions… */
-      }),
-    },
-    react({ include: /\.(jsx|js|mdx|md|tsx|ts)$/ }),
-  ],
-});
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
